8dee699faf4371d0c9aa0ae712b6a6a7b55c7aa1
Gabriela Nery <gneri94@gmail.com>
2024-04-03 22:07:46 -0300

    feat: shelter validation

--- src/shelter/dtos/update.shelter.controller.input.ts
+++ src/shelter/dtos/update.shelter.controller.input.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, Length, Matches } from "class-validator"
+
+
 export default class UpdateShelterControlerInput{
+  @IsString()
+  @IsNotEmpty()
   name:string
+  //@Matches(new RegExp(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/))
+  @IsNotEmpty()
+  @IsString()
+  @Length(10,11)
   whatsapp:string
+  @Length(10,11)
+  @IsNotEmpty()
   phone:string
+  @IsEmail()
+  @IsNotEmpty()
   email:string
 }
