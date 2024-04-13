508d7f7094148fb28d2dd8f24e593c5368cb0a5b
Gabriela Nery <gneri94@gmail.com>
2024-04-12 21:17:18 -0300

    Update create.pet.controller.input.ts

--- src/pet/dtos/create.pet.controller.input.ts
+++ src/pet/dtos/create.pet.controller.input.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsString,
@@ -7,18 +8,23 @@ import {
 export default class CreatePetControllerInput {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   type: string;
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   size: string;
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   gender: string;
   @IsString()
   @IsNotEmpty()
   @MaxLength(1024)
+  @ApiProperty()
   bio: string;
 }
